ed798e66d1ef68dd895635d187dd3bd856341182
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-06-12 09:38:24 +0530

    hero

---
+++ src/Components/ReadMore.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./ReadMore.css"
+import { IoMdArrowForward } from "react-icons/io";
+
+function ReadMore() {
+  return (
+    <div className="read-container flex justify-between">
+        <div>
+            <p className="pl-2">Read More</p>
+        </div>
+        <div className="read-box2 flex justify-center items-center">
+            <IoMdArrowForward className="text-xl" />
+        </div>
+    </div>
+  )
+}
+
+export default ReadMore
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import "./Home.css"
 import Homes from "../../../public/home.jpg"
+import ReadMore from "../../Components/ReadMore"
 
 // h-screen w-[100%] sm:h-screen sm:w-[60%]
 
@@ -10,10 +11,13 @@ function Home() {
             <div className="Home-Image overflow-hidden">
                 <img className="w-full h-screen object-cover absolute "src={Homes} alt="Hero Image"></img>
                 <div className="Home-Text relative h-screen w-[100%] sm:w-[70%] flex justify-center items-center">
-                    <div className="box1 flex flex-col text-center sm:text-start">
+                    <div className="box1 flex flex-col text-center sm:text-start space-y-3">
                         <p className="font-medium sm:text-xl">We are creative Inc.</p>
                         <p className="font-bold text-4xl sm:text-6xl">CLEAN SIMPLE AND MINIMAL</p>
-                        <p>Lorem Ipsum Lorem Impsum Lorem Ipsum</p>
+                        <p>Lorem Ipsum Lorem Impsum Lorem Ipsum Lorem Ipsum Lorem Impsum Lorem Ipsum</p>
+                        <div className="flex justify-center items-center sm:justify-start">
+                            <ReadMore />
+                        </div>
 
                     
                     </div>
