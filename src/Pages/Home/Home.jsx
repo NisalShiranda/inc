2986b169eed2d0560e618b8af835706af1d57cfd
Nisal Shiranda <nisalshiranda001@gmail.com>
2024-06-18 09:03:13 +0530

    frfrf

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
                 <img className="w-full h-screen object-cover absolute "src={Homes} alt="Hero Image"></img>
                 <div className="Home-Text relative h-screen w-[100%] sm:w-[70%] flex justify-center items-center">
                     <div className="box1 flex flex-col text-center sm:text-start space-y-3 px-5 sm:px-32">
-                        <p className="font-medium sm:text-xl">We are creative Inc. Nisal Shiranda</p>
+                        <p className="font-medium sm:text-xl">We are creative Inc. Nisal Shiranda rana</p>
                         <p className="font-bold text-4xl sm:text-6xl lg:text-9xl">CLEAN SIMPLE AND MINIMAL</p>
                         <p>Lorem Ipsum Lorem Impsum Lorem Ipsum Lorem Ipsum Lorem Impsum Lorem Ipsum</p>
                         <div className="flex justify-center items-center sm:justify-start">
